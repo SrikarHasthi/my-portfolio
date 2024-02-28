409da0b4ef535e4610af16a11080a1c7c3d34351
SrikarHasthi <hsrikar1999@gmail.com>
2024-02-27 19:47:56 -0500

    added scroll down in intro

--- src/components/Introduction/Intro.tsx
+++ src/components/Introduction/Intro.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadFull } from "tsparticles";
-
 import "../../App.scss";
 import "./Intro.scss"
 import particlesOptions from "../../particles.json";
 import { ISourceOptions } from "@tsparticles/engine";
-import ScrollAnimation from "react-animate-on-scroll";
 import { TypeAnimation } from "react-type-animation";
-import "animate.css/animate.min.css";
+import ScrollDownSvg from "../../assets/scroll-down.svg"
+import 'animate.css';
+import { NavHashLink } from "react-router-hash-link";
 
 
 const Intro = () => {
@@ -32,8 +32,7 @@ const Intro = () => {
                         options={particlesOptions as unknown as ISourceOptions}
                     />
 
-                    <div className="main-intro-text">
-                        {/* <ScrollAnimation animateIn="fadeIn"> */}
+                    <div className="main-intro-text animate__animated animate__fadeInUp">
                         <TypeAnimation
                             cursor={false}
                             sequence={[
@@ -51,34 +50,34 @@ const Intro = () => {
                                     500,
                                     'A Front-End Developer.',
                                     1000,
-                                    'Hold a Master\s Degree in Software Engineering.',
+                                    'A Master\'s Degree Graduate.',
                                     // 'I design and code beautifully simple things, and I love what I do.',
                                     1000,
                                     'A problem solver.',
                                     1000,
                                     'An innovative thinker.',
-                                    1000,
-                                    'A....',
-                                    1000,
-                                    'A.... cool guy?',
-                                    1000,
-                                    "Ok...",
-                                    1000,
-                                    "Ok...  I'm running out of ideas...",
-                                    1000,
-                                    "Uhh...",
+                                    // 1000,
+                                    // 'A....',
+                                    // 1000,
+                                    // 'A.... cool guy?',
+                                    // 1000,
+                                    // "Ok...",
+                                    // 1000,
+                                    // "Ok...  I'm running out of ideas...",
+                                    // 1000,
+                                    // "Uhh...",
                                     1000,
                                     "Uhh... you can scroll down to see my projects now...",
                                     300,
                                     () => setShowScrollDown(true),
-                                    1000,
-                                    "Seriously, my projects are really cool, go check them out!",
-                                    1000,
-                                    "You're uh...",
-                                    1000,
-                                    "You're uh... still here?",
-                                    1000,
-                                    "Ok, this has been fun, but I'm gonna restart the loop now...",
+                                    // 1000,
+                                    // "Seriously, my projects are really cool, go check them out!",
+                                    // 1000,
+                                    // "You're uh...",
+                                    // 1000,
+                                    // "You're uh... still here?",
+                                    // 1000,
+                                    // "Ok, this has been fun, but I'm gonna restart the loop now...",
                                     // 1000,
                                     // "Or...",
                                     // 1000,
@@ -93,8 +92,17 @@ const Intro = () => {
                                 repeat={Infinity}
                             />
                         }
-                        {/* </ScrollAnimation> */}
                     </div>
+                    {
+                        showScrollDown &&
+                        <div className="scroll-down-container animate__animated animate__headShake">
+                            Scroll down
+                            <img
+                                src={ScrollDownSvg}
+                                alt="scroll-down"
+                            />
+                        </div>
+                    }
 
 
                     <div className="custom-shape-divider-top-1706220601">
