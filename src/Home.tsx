0c143b03bd30f61b350179d6240c723e2651ee11
SrikarHasthi <hsrikar1999@gmail.com>
2024-03-22 12:27:58 -0400

    added particles in intro

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,55 +6,16 @@ import { Experience } from "./components/Experience/Experience";
 import { Header } from "./components/Header/Header";
 import Intro from "./components/Introduction/Intro";
 import { Project } from "./components/Project/Project";
-import scroll from './assets/scrollToTop.svg'
+
 import { Footer } from "./components/Footer/Footer";
-import { CircularProgressbar } from "react-circular-progressbar";
+
 import 'react-circular-progressbar/dist/styles.css';
 
 export const Home = () => {
 
-    const [showScrollToTop, setShowScrollToTop] = useState(false);
-    const [scrollTop, setScrollTop] = useState(0);
-
-    useEffect(() => {
-        const handleScrollButtonVisiblity = () => {
-            window.pageYOffset > 300 ? setShowScrollToTop(true) : setShowScrollToTop(false);
-        }
-
-
-        window.addEventListener('scroll', handleScrollButtonVisiblity);
-        return () => {
-            window.removeEventListener('scroll', handleScrollButtonVisiblity);
-        }
-    }, [])
-
-    const handleScrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' })
-    };
 
 
 
-    const onScroll = () => {
-        // This will calculate how many pixels the page is vertically
-        const winScroll = document.documentElement.scrollTop;
-        // This is responsible for subtracticing the total height of the page - where the users page is scrolled to
-        const height =
-            document.documentElement.scrollHeight -
-            document.documentElement.clientHeight;
-
-        // This will calculate the final total of the percentage of how much the user has scrolled.
-        const scrolled = (winScroll / height) * 100;
-
-        setScrollTop(scrolled);
-    };
-
-    useEffect(() => {
-        // Fires when the document view has been scrolled
-        window.addEventListener("scroll", onScroll);
-
-        // 
-        return () => window.removeEventListener("scroll", onScroll);
-    }, []);
 
     return (
         <div>
@@ -65,18 +26,7 @@ export const Home = () => {
             <Experience />
             <Contact />
             <Footer />
-            {
-                showScrollToTop &&
 
-                <div className="scroll-to-top" onClick={handleScrollToTop}>
-                     <CircularProgressbar value={scrollTop} className="scroll-progress-bar"/>
-                    <button>
-                        <img src={scroll} />
-                    </button>
-
-                </div>
-
-            }
         </div>
     );
 }
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Project.scss'
 import { ProjectCard } from './ProjectCard'
 import ScrollAnimation from 'react-animate-on-scroll'
 import { projectDetails } from '../../StaticData'
+import { CircularProgressbar } from "react-circular-progressbar";
+import scroll from '../../assets/scrollToTop.svg'
 
 export const Project = () => {
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScrollButtonVisiblity = () => {
+      window.pageYOffset > 300 ? setShowScrollToTop(true) : setShowScrollToTop(false);
+    }
+
+
+    window.addEventListener('scroll', handleScrollButtonVisiblity);
+    return () => {
+      window.removeEventListener('scroll', handleScrollButtonVisiblity);
+    }
+  }, [])
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  };
+
+
+
+  const onScroll = () => {
+    // This will calculate how many pixels the page is vertically
+    const winScroll = document.documentElement.scrollTop;
+    // This is responsible for subtracticing the total height of the page - where the users page is scrolled to
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    // This will calculate the final total of the percentage of how much the user has scrolled.
+    const scrolled = (winScroll / height) * 100;
+
+    setScrollTop(scrolled);
+  };
+
+  useEffect(() => {
+    // Fires when the document view has been scrolled
+    window.addEventListener("scroll", onScroll);
+
+    // 
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <div className='project-main-container' id="project">
@@ -16,15 +60,27 @@ export const Project = () => {
           projectDetails.map((e, id) => {
 
             return (
-              <div key={id} style={{overflow:"hidden"}}>
-              <ScrollAnimation animateIn="pulse">
-                <ProjectCard projectDetails={e} />
-              </ScrollAnimation>
+              <div key={id} style={{ overflow: "hidden" }}>
+                <ScrollAnimation animateIn="pulse">
+                  <ProjectCard projectDetails={e} />
+                </ScrollAnimation>
               </div>
             )
           })
         }
       </div>
+      {
+        showScrollToTop &&
+
+        <div className="scroll-to-top" onClick={handleScrollToTop}>
+          <CircularProgressbar value={scrollTop} className="scroll-progress-bar" />
+          <button>
+            <img src={scroll} />
+          </button>
+
+        </div>
+
+      }
     </div>
   )
 }
